4852640a6d49e3838f07bfd56d32b82e31e1b8d7
rodsmade <rodrigogregoldo@gmail.com>
2023-01-30 11:03:15 -0300

    changed permission for script and refactored to get argument from command line

--- Day_00/ex00/myawesomescript.sh
+++ Day_00/ex00/myawesomescript.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Only letters, numbers, '-' and '_' allowed
-curl -s bit.ly/rodsmade | grep href | cut --delimiter='"' --fields=2
+curl -s $1 | grep href | cut --delimiter='"' --fields=2
