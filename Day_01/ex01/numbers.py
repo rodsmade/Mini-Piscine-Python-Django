57a6773bee46cafe1f3260ec8ed1e5702c4101ff
rodsmade <rodrigogregoldo@gmail.com>
2023-01-31 12:18:53 -0300

    ex01 done

---
+++ Day_01/ex01/numbers.py
@@ -0,0 +1,9 @@
+def go():
+    with open("numbers.txt", "r") as file:
+        contents = file.read()
+        numbers = contents.split(",")
+        for number in numbers:
+            print(number.strip())
+
+if __name__ == '__main__':
+    go()
