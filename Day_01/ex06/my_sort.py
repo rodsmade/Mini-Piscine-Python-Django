aa148e6fe30ab2f91cdcd7f485e3ff9443c16838
rodsmade <rodrigogregoldo@gmail.com>
2023-02-01 12:49:30 -0300

    ex06 done

---
+++ Day_01/ex06/my_sort.py
@@ -0,0 +1,33 @@
+d = {
+    'Hendrix': '1942',
+    'Allman': '1946',
+    'King': '1925',
+    'Clapton': '1945',
+    'Johnson': '1911',
+    'Berry': '1926',
+    'Vaughan': '1954',
+    'Cooder': '1947',
+    'Page': '1944',
+    'Richards': '1943',
+    'Hammett': '1962',
+    'Cobain': '1967',
+    'Garcia': '1942',
+    'Beck': '1944',
+    'Santana': '1947',
+    'Ramone': '1948',
+    'White': '1975',
+    'Frusciante': '1970',
+    'Thompson': '1949',
+    'Burton': '1939',
+}
+
+
+def my_sort():
+    alphabetically_sorted = sorted(d.items())
+    sorted_rockstars = sorted(dict(alphabetically_sorted).items(), key=lambda x: x[1])
+    for entry in sorted_rockstars:
+        print(entry[1], entry[0])
+
+
+if __name__ == "__main__":
+    my_sort()
