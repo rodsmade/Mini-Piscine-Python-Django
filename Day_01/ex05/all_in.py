d4716df1adf4cc0121ecb7cdf67981bbb4f693fe
rodsmade <rodrigogregoldo@gmail.com>
2023-02-01 11:09:30 -0300

    ex05 done

---
+++ Day_01/ex05/all_in.py
@@ -0,0 +1,59 @@
+import sys
+
+states = {
+    "Oregon": "OR",
+    "Alabama": "AL",
+    "New Jersey": "NJ",
+    "Colorado": "CO"
+}
+capital_cities = {
+    "OR": "Salem",
+    "AL": "Montgomery",
+    "NJ": "Trenton",
+    "CO": "Denver"
+}
+
+def capital_city(state):
+    if (state not in states.keys()):
+        return("Unknown state")
+    else:
+        return(capital_cities[states[state]])
+
+def state(capital):
+    if (capital not in capital_cities.values()):
+        return("Unknown capital city")
+    else:
+        all_capitals = list(capital_cities.values())
+        index_of_capital = all_capitals.index(capital)
+        return(list(states.keys())[index_of_capital])
+
+def all_in():
+    if len(sys.argv) != 2:
+        return
+    terms = sys.argv[1].split(",")
+    res = []
+    for term in terms:
+        trimmed = term.strip(' ')
+        res.append(trimmed)
+
+    no_double_spaces = []
+    for element in res:
+        while '  ' in element:
+            element = element.replace('  ', ' ')
+        no_double_spaces.append(element)
+
+    for element in no_double_spaces:
+        capitalized = str(element).title()
+        if capitalized in capital_cities.values():
+            print(capitalized, "is the capital of", state(capitalized))
+        elif capitalized in states.keys():
+            print(capital_city(capitalized), "is the capital of", capitalized)
+        elif element == '':
+            print('\r', end='')
+        else:
+            print(element, "is neither a capital city nor a state")
+
+    return
+
+if __name__ == '__main__':
+    all_in()
