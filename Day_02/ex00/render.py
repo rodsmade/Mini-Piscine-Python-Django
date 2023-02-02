1571ab77954fa0535b8922c7051f409f590e47b0
rodsmade <rodrigogregoldo@gmail.com>
2023-02-02 13:56:53 -0300

    casting all setting vars to string just in case

--- Day_02/ex00/render.py
+++ Day_02/ex00/render.py
@@ -25,10 +25,10 @@ if __name__ == "__main__":
 
     with open(template_path) as template_file:
         contents = template_file.read()
-        contents = contents.replace("{name}", cv_vars.name)
-        contents = contents.replace("{surname}", cv_vars.surname)
+        contents = contents.replace("{name}", str(cv_vars.name))
+        contents = contents.replace("{surname}", str(cv_vars.surname))
         contents = contents.replace("{age}", str(cv_vars.age))
-        contents = contents.replace("{profession}", cv_vars.profession)
+        contents = contents.replace("{profession}", str(cv_vars.profession))
         
         output_file = open("cv.html", "w")
         output_file.write(contents)
