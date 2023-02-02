d9950296359dbc0b15c095442d63b2871d9642af
rodsmade <rodrigogregoldo@gmail.com>
2023-02-02 16:52:36 -0300

    ex01 boilerplate

---
+++ Day_02/ex01/intern.py
@@ -0,0 +1,24 @@
+class Intern:
+    def __init__(self, name="My name? I’m nobody, an intern, I have no name."):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+        
+    def work(self):
+        raise Exception("I’m just an intern, I can’t do that...")
+
+    class Coffee:
+        def __str__():
+            "This is the worst coffee you ever tasted."
+    
+    def make_coffee():
+        cofee = Coffee()
+        return()
+
+
+
+
+A make_coffee() method that will return an instance of the Coffee class that you will
+have implemented in Intern class.
