54fa978e82a073c1979ae7bba61b504daa413271
rodsmade <rodrigogregoldo@gmail.com>
2023-02-03 11:20:38 -0300

    implemented exception for coffee machine

--- Day_02/ex03/machine.py
+++ Day_02/ex03/machine.py
@@ -1,18 +1,34 @@
 import beverages
 
-class CoffeeMachine:
-    def __init__(self) -> None:
-        pass
 
 class EmptyCup(beverages.HotBeverage):
     def __init__(self) -> None:
         self.name = "empty cup"
         self.price = 0.90
-    
+
     def description(self) -> str:
         return ("An empty cup?! Gimme my money back!")
 
 
+class BrokenMachineException(Exception):
+    def __init__(self) -> None:
+        super().__init__("This coffee machine has to be repaired.")
+
+
+class CoffeeMachine:
+    def __init__(self) -> None:
+        pass
+
+    def test_machine(self):
+        raise BrokenMachineException
+
+
 if __name__ == "__main__":
+    machine = CoffeeMachine()
     empty_cup = EmptyCup()
     print(empty_cup)
+
+    try:
+        machine.test_machine()
+    except BrokenMachineException as e:
+        print(e)
