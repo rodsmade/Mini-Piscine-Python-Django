a0b82b412bef229aae62ff60ff9443554318e91f
rodsmade <rodrigogregoldo@gmail.com>
2023-02-03 12:01:10 -0300

    ex02 done

--- Day_02/ex03/machine.py
+++ Day_02/ex03/machine.py
@@ -1,10 +1,13 @@
 import beverages
+import random
 
 
 class EmptyCup(beverages.HotBeverage):
+    name = "empty cup"
+    price = 0.90
+
     def __init__(self) -> None:
-        self.name = "empty cup"
-        self.price = 0.90
+        pass
 
     def description(self) -> str:
         return ("An empty cup?! Gimme my money back!")
@@ -17,35 +20,36 @@ class BrokenMachineException(Exception):
 
 class CoffeeMachine:
     def __init__(self) -> None:
-        pass
-
-    def test_machine(self):
-        raise BrokenMachineException
+        self.serve_counter = 0
+        self.repaired = True
 
     def repair(self):
-        print("This machine is under repair. Please come again later!")
+        self.repair = True
+        self.serve_counter = 0
+        print("This machine has been repaired. Come again!\n")
 
     def serve(self, hot_beverage):
-        print("Serving a cup of {} for {:.2f}".format(hot_beverage.description(), hot_beverage.price))
+        if self.serve_counter < 10:
+            random_options = [hot_beverage, EmptyCup]
+            beverage_class = random.choice(random_options)
+            self.serve_counter += 1
+            return beverage_class()
+        else:
+            self.repaired = False
+            raise BrokenMachineException
 
 
 if __name__ == "__main__":
     machine = CoffeeMachine()
-    empty_cup = EmptyCup()
-    print(empty_cup)
+    beverage_classes = [beverages.Coffee, beverages.Tea, beverages.Cappuccino, beverages.Chocolate]
 
     try:
-        machine.test_machine()
+        for i in range(15):
+            print("------------------------------------ Drink No.: ", i + 1, "\n", machine.serve(beverage_classes[i % 4]), '\n', sep="")
     except BrokenMachineException as e:
         print(e)
-
-    coffee = beverages.Coffee()
-    machine.serve(coffee)
-
-    tea = beverages.Tea()
-    machine.serve(tea)
     
-    chocolate = beverages.Chocolate()
-    machine.serve(chocolate)
-
     machine.repair()
+
+    for i in range(10):
+        print("------------------------------------ Drink No.: ", i + 1, "\n", machine.serve(beverage_classes[i % 4]), '\n', sep="")
