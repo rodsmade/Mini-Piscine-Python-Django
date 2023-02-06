bc3c9f07d43ae74ae6ba6eb6a5503f3df9e7b960
rodsmade <rodrigogregoldo@gmail.com>
2023-02-06 09:51:41 -0300

    last touches for submission

--- Day_02/ex00/render.py
+++ Day_02/ex00/render.py
@@ -14,18 +14,12 @@ if __name__ == "__main__":
     template_path = get_template_path()
     template_contents = open(template_path, 'r').read()
 
-#     contents = contents.replace("{name}", str(cv_vars.name))
-#     contents = contents.replace("{surname}", str(cv_vars.surname))
-#     contents = contents.replace("{age}", str(cv_vars.age))
-#     contents = contents.replace("{profession}", str(cv_vars.profession))
-
-
     settings_contents = open("settings.py", "r")
 
     setting_variables = {}
     for line in settings_contents:
         key_value = line.split(" = ")
-        template_contents = template_contents.replace("{" + key_value[0] + "}", key_value[1].strip('"'))
+        template_contents = template_contents.replace("{" + key_value[0] + "}", key_value[1].strip('"\n'))
     
     output_file = open("cv.html", "w")
     output_file.write(template_contents)
--- Day_02/ex00/settings.py
+++ Day_02/ex00/settings.py
@@ -1,6 +1,6 @@
-name = "gilmar"
-surname = "romani"
-position = "junior"
+name = "Gilmar"
+surname = "Romani"
+position = "Junior Software Engineer"
 age = 24
 telephone = "9999-9999"
 email = "gilmar.romani@gmail.com"
