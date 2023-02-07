4c3e91406f61c9f774e54579aa378e458647dd3b
rodsmade <rodrigogregoldo@gmail.com>
2023-02-07 11:00:52 -0300

    ex01 done

--- Day_03/ex01/my_program.py
+++ Day_03/ex01/my_program.py
@@ -1,4 +1,9 @@
 from local_lib.path import Path
 
 if __name__ == "__main__":
-    print(Path())
+    folder = Path("myfolder")
+    folder.mkdir_p()
+    file = folder / "myfile.txt"
+    file.touch()
+    file.write_text("FOOBAR HAHAA")
+    print(file.read_bytes().decode("utf-8"))
