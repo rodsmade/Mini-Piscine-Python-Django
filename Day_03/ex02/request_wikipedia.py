49fe6e5886ff7956f7e4931afcc4ddca8c350f54
rodsmade <rodrigogregoldo@gmail.com>
2023-02-07 17:45:23 -0300

    WIP: request to wikipedia endpoint

--- Day_03/ex02/request_wikipedia.py
+++ Day_03/ex02/request_wikipedia.py
@@ -4,4 +4,10 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Error: pass one string to be used as the search term.")
         sys.exit(-42)
-    print(sys.argv[1])
+    search_term = sys.argv[1]
+
+    response = requests.get("https://en.wikipedia.org/w/api.php?action=parse&page=Fleet_foxes&format=json")
+    print(response.content.decode('utf-8'))
+
+    # só abrir o arquivo se der bom, ou seja, não criar se  parameter absence, wrong parameter, invalid request, information not found, server problem or any other problem
+    # open(search_term + ".wiki", 'w')
