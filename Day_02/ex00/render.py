522a9d60283e3a61b2f56e464057016201fd1b58
rodsmade <rodrigogregoldo@gmail.com>
2023-02-07 16:43:25 -0300

    fixed ex00

--- Day_02/ex00/render.py
+++ Day_02/ex00/render.py
@@ -1,11 +1,12 @@
-import settings as cv_vars
 import sys
 
 def get_template_path():
     if len(sys.argv) != 2:
+        print("Error. Pass in only one argument, e.g.: `python3 render.py myCV.template`")
         sys.exit()
 
     if not sys.argv[1].endswith(".template"):
+        print("Error. File must end in `.template`, e.g.: `myCV.template`")
         sys.exit()
 
     return sys.argv[1]
@@ -15,12 +16,12 @@ if __name__ == "__main__":
     template_contents = open(template_path, 'r').read()
 
     settings_contents = open("settings.py", "r")
-
     setting_variables = {}
     for line in settings_contents:
-        key_value = line.split(" = ")
-        template_contents = template_contents.replace("{" + key_value[0] + "}", key_value[1].strip('"\n'))
-    
+        if line[0] != '#':
+            key_value = line.split(" = ")
+            template_contents = template_contents.replace("{" + key_value[0] + "}", key_value[1].strip('"\n'))
+
     output_file = open(sys.argv[1].split('.')[0] + ".html", "w")
     output_file.write(template_contents)
     output_file.close()
--- Day_02/ex00/settings.py
+++ Day_02/ex00/settings.py
@@ -6,3 +6,4 @@ telephone = "9999-9999"
 email = "gilmar.romani@gmail.com"
 grades = 100.00
 school = 42
+title = "PAGE TITLE MFR"
