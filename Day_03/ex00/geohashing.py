d02ea4655d7b3aedee8ae8a19a18ff5e3fb6926a
rodsmade <rodrigogregoldo@gmail.com>
2023-02-08 12:22:00 -0300

    added validation for latitude and longitude ranges

--- Day_03/ex00/geohashing.py
+++ Day_03/ex00/geohashing.py
@@ -7,8 +7,16 @@ def validate_args():
         print("E.g.:\tpython3 geohashing.py 37.421542 -122.085589 2005-05-26-10458.68")
         exit(-42)
 
-    latitude = float(sys.argv[1])
-    longitude = float(sys.argv[2])
+    try:
+        latitude = float(sys.argv[1])
+        longitude = float(sys.argv[2])
+    except ValueError:
+        sys.exit("Wrong format for latitude/longitude. Fix yo' shi'!")
+     
+    if abs(latitude) > 90.0:
+         exit("Latitude out of range! Must be between -90 and +90")
+    if abs(longitude) > 180.0:
+         exit("Longitude out of range! Must be between -180 and +180")
     datedow = bytes(sys.argv[3], 'ascii')
     antigravity.geohash(latitude=latitude, longitude=longitude, datedow=datedow)
 
