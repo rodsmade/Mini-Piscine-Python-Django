a7859f97851e88904cf0c496d8ebe8b49c10bf40
rodsmade <rodrigogregoldo@gmail.com>
2023-02-08 11:52:48 -0300

    managed to use dewiki to print a page from wikipedia retrieved via the api into human readable format

--- Day_03/ex02/request_wikipedia.py
+++ Day_03/ex02/request_wikipedia.py
@@ -1,13 +1,40 @@
-import requests, json, sys
+import requests
+import json
+import sys
+import dewiki
+
+
+def get_page_as_wikitext(page_title):
+    API_ENDPOINT = "https://en.wikipedia.org/w/api.php"
+    params = {
+        'action': 'parse',
+        'page': page_title,
+        'prop': 'wikitext',
+        'format': 'json'
+    }
+    response = requests.get(API_ENDPOINT, params=params)
+    if response.status_code != 200:
+        print("request deu ruim")
+        print(response.text)
+        return None
+    else:
+        # print(response.text)
+        data = json.loads(response.text)['parse']['wikitext']['*']
+        return data
+
+
+def dewiki_wikitext(text):
+    return dewiki.from_string(text).strip()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Error: pass one string to be used as the search term.")
         sys.exit(-42)
+
     search_term = sys.argv[1]
+    wikitext = get_page_as_wikitext(search_term)
+    dewikied_page = dewiki_wikitext(wikitext)
 
-    response = requests.get("https://en.wikipedia.org/w/api.php?action=parse&page=Fleet_foxes&format=json")
-    print(response.content.decode('utf-8'))
-
-    # só abrir o arquivo se der bom, ou seja, não criar se  parameter absence, wrong parameter, invalid request, information not found, server problem or any other problem
-    # open(search_term + ".wiki", 'w')
+    output = open(search_term + ".wiki", 'w')
+    output.write(dewikied_page)
