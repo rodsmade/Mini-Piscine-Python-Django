3d1558cce73b60a9db9c858394a6b0e62b5eb461
rodsmade <rodrigogregoldo@gmail.com>
2023-02-09 14:42:48 -0300

    ex03 done

--- Day_03/ex03/roads_to_philosophy.py
+++ Day_03/ex03/roads_to_philosophy.py
@@ -2,53 +2,48 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-def find_first_link_in_page(page_title):
+
+def road_to_philosophy(page_title, pages_visited):
     URL = "https://en.wikipedia.org/wiki/" + page_title
-    
+
     try:
         response = requests.get(URL)
+        response.raise_for_status()  # if there's redirects, follow up
+        soup = BeautifulSoup(response.content, "html.parser")
+        page_title = soup.find(name="title").text.split(" - ")[0]
+        all_links = soup.find(id="mw-content-text").select("p > a")
+
+        if len(all_links) == 0:
+            sys.exit("It leads to a dead end !")
+        if page_title in pages_visited:
+            sys.exit("It leads to an infinite loop !")
+        if page_title == "Philosophy":
+            print("Philosophy")
+            return
+
+        print(page_title)
+        pages_visited.append(page_title)
+
+        for link in all_links:
+            attributes = str(link).split(' ')
+            for attribute in attributes:
+                if attribute.startswith('href'):
+                    next_search_term = attribute.split('=')[1].rsplit('/', 1)[-1].strip('"')
+                    road_to_philosophy(next_search_term, pages_visited)
+                    return
+
     except requests.exceptions.RequestException:
         return None
-    
-    soup = BeautifulSoup(response.content, "html.parser")
-    all_links = soup.find(id="mw-content-text").select("p > a")
 
-    if len(all_links) == 0:
-        return None
-
-    for item in all_links:
-        attributes = str(item).split(' ')
-        for attribute in attributes:
-            if attribute.startswith('href'):
-                next_page_title = attribute.split('=')[1]
-                print("next page title:", next_page_title.rsplit('/', 1)[-1])
-                return next_page_title.rsplit('/', 1)[-1]
-
-    return None
-    
 
 if __name__ == "__main__":
-    stop_loop = False
-    pages_visited = []
-    
-    count = 0
-    
     if len(sys.argv) != 2:
         sys.exit("Pass in only one argument, use double quotes if necessary")
-    
-    page_title = sys.argv[1].replace(' ', '_')
-    
-    while not stop_loop:
-        pages_visited.append(page_title)
-        first_link_referenced_in_page = find_first_link_in_page(page_title)
-        
-        if not first_link_referenced_in_page:
-            sys.exit("It leads to a dead end !")
-        if first_link_referenced_in_page in pages_visited:
-            sys.exit("It leads to an infinite loop !")
-        
-        print(page_title)
-        page_title = first_link_referenced_in_page
-        count += 1
-        if count == 3:
-            break
+    initial_search_term = sys.argv[1].replace(' ', '_')
+
+    pages_visited = []
+
+    road_to_philosophy(initial_search_term, pages_visited)
+
+    print((str(len(pages_visited)) + " roads from " +
+          initial_search_term + " to philosophy"))
